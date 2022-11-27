e59ab1e620b2f419a91665016e83693ca3c5e038
Olivier Sprangers <osprangers@gmail.com>
2022-11-27 15:00:36 +0100

    testwheelsupdate

--- .github/scripts/test_wheels.sh
+++ .github/scripts/test_wheels.sh
@@ -3,7 +3,11 @@
 set -e
 set -x
 pwd
-cd ../../
-cd tests
+where pip
+INVENV=$(python -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
+# cd ../../
+# cd tests
 
-python -m pytest 
+# python -m pip install wheelhouse/*.whl
+# python -m pytest 
+pytest $1/tests
