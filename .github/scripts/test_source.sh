58dc354c3048ddf8eb8dc1752f0113d08290fe2d
Olivier Sprangers <osprangers@gmail.com>
2023-03-20 15:43:37 +0100

    add_lightgbm_as_test_depedendency

--- .github/scripts/test_source.sh
+++ .github/scripts/test_source.sh
@@ -9,10 +9,8 @@ python -m venv test_env
 source test_env/bin/activate
 
 python -m pip install pgbm/pgbm/dist/*.tar.gz
-python -m pip install pytest numba torch --extra-index-url https://download.pytorch.org/whl/cpu ninja
+python -m pip install pytest numba torch --extra-index-url https://download.pytorch.org/whl/cpu ninja lightgbm
 # Run the tests on the installed source distribution
-
-# python pgbm/pgbm/tests/test_training.py
 mkdir tmp_for_test
 cd tmp_for_test
 pwd
--- .github/scripts/test_wheels.sh
+++ .github/scripts/test_wheels.sh
@@ -3,14 +3,7 @@
 set -e
 set -x
 pwd
-# where pip
 INVENV=$(python -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
-# cd ../../
-# cd tests
 
-# python -m pip install wheelhouse/*.whl
-# python -m pytest 
-# pytest $1/sklearn/tests
-# pytest $1/torch/tests
 pytest --pyargs pgbm.sklearn
 pytest --pyargs pgbm.torch
